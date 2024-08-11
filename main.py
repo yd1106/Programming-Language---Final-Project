0c734676a74b3b6b746d885f2c761e0043648f6a
yd1106 <yd1106@gmail.com>
2024-08-11 16:27:02 +0300

    check check

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# This is a sample Python script.
+# This is a sample Python script.A
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
@@ -6,7 +6,7 @@
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Hi BI, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
